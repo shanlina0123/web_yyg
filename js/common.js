1392549031babe704209f20865beeaa4c55047d6
zhaoying111 <2864342582@qq.com>
2018-06-22 16:58:27 +0800

    图片添加

--- js/common.js
+++ js/common.js
@@ -6,7 +6,29 @@ $(".rightlink li a").hover(function() {
 });
 //向上点击事件
 $(".gotoplink").click(function() {
-    $("html,body").animate({
-        "scrollTop": '0'
-    }, 500);
-})
+        $("html,body").animate({
+            "scrollTop": '0'
+        }, 500);
+    })
+    //轮播
+function slide(name, width, height) {
+    if ($(name).children().length > 1) {
+        $(name).slidesjs({
+            width: width,
+            height: height,
+            navigation: false,
+            play: {
+                auto: true,
+                restartDelay: 2000
+            },
+            callback: {
+                complete: function(number) {
+                    clearTimeout(slideTimer)
+                    var slideTimer = setTimeout(function() {
+                        $(".slidesjs-play").click();
+                    }, 5000)
+                }
+            }
+        });
+    }
+}
