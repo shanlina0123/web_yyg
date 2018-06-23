8b55e58ad68d07da2c6f7977b9ae87160eaa5dce
zhaoying111 <2864342582@qq.com>
2018-06-23 11:46:40 +0800

    动态效果添加

--- js/common.js
+++ js/common.js
@@ -1,9 +1,9 @@
 //右侧划上事件
-$(".rightlink li a").hover(function() {
-    $(this).siblings().show()
-}, function() {
-    $(this).siblings().hide()
-});
+// $(".rightlink li a").hover(function() {
+//     $(this).siblings().show()
+// }, function() {
+//     $(this).siblings().hide()
+// });
 //向上点击事件
 $(".gotoplink").click(function() {
         $("html,body").animate({
