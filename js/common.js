b4882e90ce8acd81eec3164fe7e97b0882664178
zhaoying111 <2864342582@qq.com>
2018-06-27 11:29:34 +0800

    新闻列表。详情和产品页面调整

--- js/common.js
+++ js/common.js
@@ -10,7 +10,33 @@ $(".gotoplink").click(function() {
             "scrollTop": '0'
         }, 500);
     })
-    //轮播
+    //菜单点击效果
+$("header .navbtn").click(function() {
+    if ($(".navwrap").css("display") === "none") {
+        $(".navwrap").slideDown();
+        $("body").height($(window).height()).css("overflow", "hidden");
+    } else {
+        $(".navwrap").slideUp();
+        $("body").css("overflow", "auto");
+        $("header .navwrap .nav .subnav").hide()
+    }
+});
+//二级导航点击效果
+$("header .navwrap .nav li").click(function() {
+        event.stopPropagation();
+        $(this).find(".subnav").slideToggle();
+    })
+    //PC登录提示
+$(".linkbtn").click(function() {
+    $(".forpcpop").show().delay(2000).hide(300);
+});
+$(window).click(function(e) {
+    if (!$(e.target).is(".navbtn") && !$(e.target).is(".nav")) {
+        $(".navwrap").slideUp();
+        $("body").css("overflow", "auto");
+    }
+});
+//轮播
 function slide(name, width, height) {
     if ($(name).children().length > 1) {
         $(name).slidesjs({
